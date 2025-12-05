96fce546339e1f015dc6d5444c1b3f4555b0192d
Rollan <sarsembaevrollan05@gmail.com>
2025-12-05 17:59:05 +0500

    test api

--- src/ai/ai.service.ts
+++ src/ai/ai.service.ts
@@ -3,7 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AiService {
   async ask(message: string) {
-    // Здесь ты можешь добавить любую логику
     return {
       reply: `Вы сказали: ${message}`,
     };
--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,5 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
   await app.listen(3000);
-  console.log(`Server started on http://localhost:3001`);
 }
 bootstrap();
